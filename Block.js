d662668c708ebf5d1419645e1e83605dd25e7aaf
Elena Kazakova <61557802+N0vum@users.noreply.github.com>
2022-03-30 18:06:09 +0700

    Update Block.js
    added coponent Button

--- Block.js
+++ Block.js
@@ -1,20 +1,24 @@
 import React, { useRef } from "react";
 import Card from "./Card";
 import useHover from "./useHover";
+import Button from "./Button";
 
-const Block = ({ data }) => {
+const Block = ({ data, signs }) => {
   const { h2, ...pairOfHeroes } = data;
   const blockRef = useRef();
-  const isHovered = useHover(blockRef);
+  const {isHovered, png} = useHover(blockRef);
 
   return (
-    <div>
-      <p>{h2}</p>
-      <div className="block" ref={blockRef}>
+    <div ref={blockRef}>
+      <h3>{h2}</h3>
+            <div className="block" >
         {Object.entries(pairOfHeroes).map((hero) => (
-          <Card key={hero[0]} data={hero[1]} isHovered={isHovered} />
-        ))}
+          <div>
+          <label><Card key={hero[0]} data={hero[1]} isHovered={isHovered} png={png} signs={signs} /></label>
+                   </div>
+            ))}
       </div>
+      <Button data={data} isHovered={isHovered} png={png} signs={signs} />
     </div>
   );
 };
