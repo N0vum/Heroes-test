4b415d377be56eee4066e2036356766801290f19
Elena Kazakova <61557802+N0vum@users.noreply.github.com>
2022-03-31 17:46:24 +0700

    Add files via upload
    added component

---
+++ Result.js
@@ -0,0 +1,54 @@
+import React, { useState, useRef, useEffect } from "react";
+
+const Result = ({ signs }) => {
+  const [result, setResult] = useState(null);
+  const resultRef = useRef();
+  
+  function getTypes() {
+     let code=signs.join("").toUpperCase()
+  console.log(code)
+   fetch("https://raw.githubusercontent.com/N0vum/Heroes-test/main/Types")
+      .then((response) => response.json())
+      .then((data) =>
+        setResult(
+          <div className="result">
+            <h1>{data[code].role}</h1>
+            <p>{data[code].description}</p>
+          </div>
+        )
+      )
+   
+  }
+  function turnOut() {
+     setResult(null);
+     signs.length = 0;
+     window.scrollTo(0, 0);
+    window.location.reload() //насколько это затратно? 
+    //Как обнулять переменную code с помощью замыкания без обновления страницы?
+    }
+
+  useEffect(() => {
+    if (!resultRef) {
+      return;
+    }
+    if (result != null)
+      resultRef.current.scrollIntoView({ block: "start", behavior: "smooth" });
+    
+  }, [result]);
+
+  return (
+    <div ref={resultRef}>
+      <div className="btnsRes">
+        <button className="res" onClick={getTypes}>
+          Result
+        </button>
+        <button className="res" onClick={turnOut}>
+          Reset
+        </button>
+      </div>
+      <div className="showResult">{result}</div>
+    </div>
+  );
+};
+
+export default Result;
