333aea937611e0419deb195c3bd8ea4d53bf4de4
Elena Kazakova <61557802+N0vum@users.noreply.github.com>
2022-04-04 09:03:40 +0700

    Update App.js
    added Result component

--- App.js
+++ App.js
@@ -2,15 +2,18 @@ import React from "react";
 import Block from "./Block";
 import heroes from "./Heroes";
 import coloredTitle from "./Title";
+import Result from "./Result";
 
 function App() {
+const signs=[];
 
   return (
     <div className="main">
       <h1><pre>{coloredTitle}</pre></h1>
       {Object.entries(heroes).map((block) => (
-        <Block key={block[0]} data={block[1]} />
+        <Block key={block[0]} data={block[1]} signs={signs}  />
       ))}
+      <Result signs={signs} />
     </div>
   );
 }
