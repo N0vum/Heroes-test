077ff989d22889041c739770ca64ff7042464699
Elena Kazakova <61557802+N0vum@users.noreply.github.com>
2022-04-12 09:48:17 +0700

    Add files via upload
    'heroes' oject in json format

---
+++ heroesData.js
@@ -0,0 +1,63 @@
+const heroesData = {
+  "energy":{
+     "title":"Where do you get your energy?",
+     "hero1":{
+        "name":"yoda",
+        "gif":"https://c.tenor.com/XO0nQRb4xe8AAAAd/meditation-grogu.gif",
+        "png":"https://img.icons8.com/color/55/000000/yoda.png",
+        "mark":"e"
+     },
+     "hero2":{
+        "name":"thor",
+        "gif":"https://c.tenor.com/nLqcQPtAKoQAAAAC/thor-avenger.gif",
+        "png":"https://img.icons8.com/color/55/000000/thor.png",
+        "mark":"i"
+     }
+  },
+  "info":{
+     "tittle":"How do you take an informstion?",
+     "hero1":{
+        "name":"spiderMan",
+        "gif":"https://c.tenor.com/jFKRQRFlYgcAAAAC/spiderman-ps4.gif",
+        "png":"https://img.icons8.com/color/55/000000/spiderman-head.png",
+        "mark":"s"
+     },
+     "hero2":{
+        "name":"galadriel",
+        "gif":"https://c.tenor.com/kF7uFoRy240AAAAC/observando-olhando.gif",
+        "png":"https://img.icons8.com/color/55/000000/legolas.png",
+        "mark":"n"
+     }
+  },
+  "decision":{
+     "title":"How do you make decisions?",
+     "hero1":{
+        "name":"spock",
+        "gif":"https://c.tenor.com/yv76Xc2IjlAAAAAC/mr-spok-spok-thinking.gif",
+        "png":"https://img.icons8.com/color/55/000000/vulcan-head.png",
+        "mark":"t"
+     },
+     "hero2":{
+        "name":"cartman",
+        "gif":"https://c.tenor.com/6HIfYFjFsm4AAAAC/cartman-chocolate.gif",
+        "png":"https://img.icons8.com/color/55/000000/eric-cartman.png",
+        "mark":"f"
+     }
+  },
+  "organisation":{
+     "title":"How do you organize your world?",
+     "hero1":{
+        "name":"hermion",
+        "gif":"https://c.tenor.com/6yFra1Ryz5IAAAAC/hermione.gif",
+        "png":"https://img.icons8.com/color/55/000000/hermione-granger-doll.png",
+        "mark":"j"
+     },
+     "hero2":{
+        "name":"homer",
+        "gif":"https://c.tenor.com/9bLLA_kKLNQAAAAM/simpsons-homer-simpson.gif",
+        "png":"https://img.icons8.com/color/55/000000/homer-simpson.png",
+        "mark":"p"
+     }
+  }
+}
+export default heroesData;
